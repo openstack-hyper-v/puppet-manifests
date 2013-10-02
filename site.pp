4006f1c598114d53bcb95a485d5e372016e50271
Peter Pouliot <ppouliot@gmail.com>
2013-10-02 15:32:52 -0400

    adding kvm-compute, neutron controller, and openstack-controller to site manifest

--- site.pp
+++ site.pp
@@ -7,3 +7,8 @@ node /^(norman|mother|ns[0-9\.]+)/ {
 node /quartermaster.*/ {
   class {'quartermaster':}
 }
+
+node /^(kvm-compute-[0-9]|neutron-controller|openstack-controller+)/ {
+  notify {"OpenStack Node: ${hostname}":}
+  class {'basenode':}
+}
