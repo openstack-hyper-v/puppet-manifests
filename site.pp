2b8ed935bf417a85da803c1cb1300af16e007991
primeministerp <primeministerpete@gmail.com>
2013-12-16 16:43:45 -0500

    packstack node defs

--- site.pp
+++ site.pp
@@ -407,7 +407,29 @@ node /^(openstack-controller).*/{
   class{'basenode':}  
 #  class{'basenode::dhcp2static':}  
   class{'jenkins::slave': }
-#  class{'packstack:'}  
+  class {'packstack':
+    openstack_release => 'havana',
+    controller_host   => '10.21.7.41',
+    network_host      => '10.21.7.42',
+    kvm_compute_host  => '10.21.7.31,10.21.7.32,10.21.7.33,10.21.7.34,10.21.7.35,10.21.7.36,10.21.7.38',
+  }
+
+  vcsrepo {'/usr/local/src/openstack-imaging-tools':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/cloudbase/windows-openstack-imaging-tools.git'
+  }
+  vcsrepo {'/usr/local/src/openstack-dev-scripts':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/ppouliot/openstack-dev-scripts.git',
+  }
+  vcsrepo {'/usr/local/src/unattend-setup-scripts':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/cloudbase/unattended-setup-scripts.git',
+  }
+
 }
 node /^(network-controller).*/{
   class{'basenode':}  
