b627febaba4e676a97cd4cdee64e0f38de3e08c8
primeministerp <primeministerpete@gmail.com>
2014-01-21 13:58:52 -0500

    semi working sensu server

--- site.pp
+++ site.pp
@@ -958,6 +958,8 @@ node /sauron.*/{
 
   class {'rabbitmq':
     delete_guest_user => true,
+    default_user => '',
+    default_pass => '',
     ssl               => true,
     ssl_cacert        => '/etc/rabbitmq/ssl/cacert.pem',
     ssl_cert          => '/etc/rabbitmq/ssl/cert.pem',
@@ -972,6 +974,11 @@ node /sauron.*/{
     ensure => present,
   }
 
+  rabbitmq_user_permissions { 'sensu@sensu':
+    configure_permission => '.*',
+    read_permission      => '.*',
+    write_permission     => '.*',
+  }
 
   class {'redis':}
 
@@ -991,35 +998,49 @@ node /sauron.*/{
     cwd     => '/tmp/ssl_certs',
     require => [Exec['get-sensu-ca-scripts'],File['/tmp/ssl_certs']],
     logoutput => true,
+    unless    => '/usr/bin/file /tmp/ssl_certs/sensu_ca/cacert.pem',
   }
 
-  file {'/etc/rabbitmq/ssl/cacert.pem':
+  file {['/etc/rabbitmq/ssl/cacert.pem','/etc/sensu/ssl/cacert.pem']:
     ensure => present,
     source => '/tmp/ssl_certs/sensu_ca/cacert.pem',
     require => [File['/etc/rabbitmq/ssl'],Exec['create-sensu-certs']],
+    owner => 'sensu',
+    group => 'sensu',
+    mode  => '0644',
   }
-  file {'/etc/rabbitmq/ssl/cert.pem':
+  file {['/etc/rabbitmq/ssl/cert.pem','/etc/sensu/ssl/cert.pem']:
     ensure => present,
     source => '/tmp/ssl_certs/server/cert.pem',
     require => [File['/etc/rabbitmq/ssl'],Exec['create-sensu-certs']],
+    owner => 'sensu',
+    group => 'sensu',
+    mode  => '0644',
   }
-  file {'/etc/rabbitmq/ssl/key.pem':
+  file {['/etc/rabbitmq/ssl/key.pem','/etc/sensu/ssl/key.pem']:
     ensure => present,
     source => '/tmp/ssl_certs/server/key.pem',
     require => [File['/etc/rabbitmq/ssl'],Exec['create-sensu-certs']],
+    owner => 'sensu',
+    group => 'sensu',
+    mode  => '0644',
   }
 
 
 
 
 
-#  class{'sensu':
-#    rabbitmq_password => 'sensu',
-#    server    => true,
-#    dashboard => true,
-#    api       => true,
+  class{'sensu':
+    rabbitmq_password => 'sensu',
+    rabbitmq_host => $fqdn,
+    rabbitmq_ssl_cert_chain => '/etc/sensu/ssl/cert.pem',
+    rabbitmq_ssl_private_key => '/etc/sensu/ssl/key.pem',
+    server    => true,
+    dashboard => true,
+    api       => true,
+    client    => false,
 #    safe_mode => true,
-#  }
+  }
 }
 node /sandbox0[1-3].*/{
   notify {"welcome ${fqdn}":}
