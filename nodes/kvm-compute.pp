0fbaf9b362502cd0c735903dc905e42556eb6f0c
Tim Rogers <tiroger@microsoft.com>
2014-03-04 12:18:35 -0800

    updates to the kvm node definition

--- nodes/kvm-compute.pp
+++ nodes/kvm-compute.pp
@@ -1,4 +1,4 @@
-node /^(kvm-compute[0-9][0-9])\.openstack\.tld$/{
+node /^(kvm-compute[0-9]+)\.openstack\.tld$/{
   class{'basenode':}  
   class{'dell_openmanage':}
   class{'sensu_server::client':}
@@ -11,7 +11,7 @@ node /^(kvm-compute[0-9][0-9])\.openstack\.tld$/{
     openstack_release => 'havana',
     controller_host   => '10.21.7.41',
     network_host      => '10.21.7.42',
-    kvm_compute_host  => '10.21.7.31,10.21.7.32,10.21.7.33,10.21.7.34,10.21.7.35,10.21.7.36,10.21.7.38',
+    kvm_compute_host  => "${ipaddress}",
   }
   case $hostname {
     'kvm-compute01',
@@ -24,14 +24,18 @@ node /^(kvm-compute[0-9][0-9])\.openstack\.tld$/{
     'kvm-compute07',
     'kvm-compute08',
     'kvm-compute09',
-    'kvm-compute10':
+    'kvm-compute10',
+    'kvm-compute11':
         { $data_interface = 'eth1' }
     default: 
         { notify {"This isn't for ${hostname}":}
     }
   }
   case $hostname {
-    'kvm-compute08','kvm-compute09','kvm-compute10':{
+    'kvm-compute08',
+    'kvm-compute09',
+    'kvm-compute10',
+    'kvm-compute11':{
        file {'/etc/nova':
        ensure  => directory,
        recurse => true,
@@ -41,20 +45,22 @@ node /^(kvm-compute[0-9][0-9])\.openstack\.tld$/{
        source  => 'puppet:///extra_files/nova',
      }
 
-     file_line {
+     ini_setting {
       'vncserver_listen':
         path   => '/etc/nova/nova.conf',
-        match  => 'vncserver_listen=10\.21\.7\.*',
-        line   => "vncserver_listen=${ipaddress_eth0}",
+        section => 'DEFAULT',
+        setting => 'vncserver_listen',
+        line   => "${ipaddress_eth0}",
         ensure => present,
         require => File['/etc/nova'],
      }
      
-     file_line {
+     ini_setting {
       'vncserver_proxyclient_address':
         path   => '/etc/nova/nova.conf',
-        match  => 'vncserver_proxyclient_address=10\.21\.7\.*',
-        line   => "vncserver_proxyclient_address=${ipaddress_eth0}",
+        section => 'DEFAULT',
+        setting => 'vncserver_proxyclient_address',
+        line   => "${ipaddress_eth0}",
         ensure => present,
         require => File['/etc/nova'],
      }
@@ -65,10 +71,11 @@ node /^(kvm-compute[0-9][0-9])\.openstack\.tld$/{
        owner   => 'root',
        group   => 'root',
        mode    => '0755',
-       source  => 'puppet:///extra_files/eth1-neutron/neutron',
+       source  => 'puppet:///extra_files/${data_interface}-neutron/neutron',
      }
     }
-     default: { notify {"This isn't for ${hostname}":}
+     default: { 
+       #notify {"This isn't for ${hostname}":}
     }
   }
   file {"/etc/sysconfig/network-scripts/ifcfg-${data_interface}":
@@ -78,27 +85,42 @@ node /^(kvm-compute[0-9][0-9])\.openstack\.tld$/{
     mode   => '0644',
     source => "puppet:///modules/packstack/ifcfg-${data_interface}",
   }
-#  package {['openstack-nova-compute',
-#            'openstack-selinux',
-#            'openstack-neutron-openvswitch',
-#            'openstack-neutron-linuxbridge',
-#            'python-slip',
-#            'python-slip-dbus',
-#            'libglade2',
-#            'nagios-common',
-#            'tuned',
-#            'yum-plugin-priorities',
-#            'system-config-firewall',
-#            'telnet',
-#            'nrpe',
-#            'centos-release-xen',
-#            'openstack-ceilometer-compute'] :
-#
-#    ensure => 'present',
-#  }
+  package {['openstack-nova-compute',
+            'openstack-selinux',
+            'openstack-neutron-openvswitch',
+            'openstack-neutron-linuxbridge',
+            'python-slip',
+            'python-slip-dbus',
+            'libglade2',
+            'nagios-common',
+            'tuned',
+            'yum-plugin-priorities',
+            'system-config-firewall',
+            'telnet',
+            'nrpe',
+            'centos-release-xen',
+            'openstack-ceilometer-compute'] :
+
+    ensure => 'present',
+  }
 #  exec {'centos_release_xen_update':
 #    command   => "/usr/bin/yum update -y --disablerepo=* --enablerepo=Xen4CentOS kernel",
 #    logoutput => true,
 #    timeout   => 0,
 #  }
+
+# No longer going to ensure these via puppet.  Will instead monitor via Sensu to facilitate investigation, as these should never halt.
+#  service {
+#    'libvirtd',
+#    'openvswitch',
+#    'openstack-nova-compute',
+#    'neutron-linuxbridge-agent':
+#    ensure  => running,
+#    require => [
+#      File["/etc/sysconfig/network-scripts/ifcfg-${data_interface}"],
+#      #Exec['centos_release_xen_update'],
+#      Class['packstack'],
+#      ],
+#  }
+
 }
