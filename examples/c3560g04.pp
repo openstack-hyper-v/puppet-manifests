1fecbe3133337447000abd820cc6d09579704b84
hypervci <hyper-v_ci@microsoft.com>
2016-03-16 13:55:18 +0000

    lint cleanup

--- examples/c3560g04.pp
+++ examples/c3560g04.pp
@@ -1,4 +1,4 @@
-# == c3560g01
+# == c3560g04
 # !!! ****************** WARNING ******************* !!!
 # !!! This resource is managed by puppet.  Any       !!!
 # !!! changes will be overwritten automatically.     !!!
